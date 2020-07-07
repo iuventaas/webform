6c2d639352fd23eaaa7344818f98399af91e80c8
iuventaas <55257470+iuventaas@users.noreply.github.com>
2020-07-07 23:20:32 +0300

    Update forms.py

--- webform/mysite/web/forms.py
+++ webform/mysite/web/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class AddForm(forms.Form):
-    item = forms.CharField(label='Item', max_length=100)
-    value = forms.CharField(label='Value', max_length=100)
+    item = forms.CharField(label='Item', max_length=100)[2:-1]
+    value = forms.CharField(label='Value', max_length=100)[2:-1]
