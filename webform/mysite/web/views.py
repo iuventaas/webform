da3b6cb262bc83e5bbfb65a97c27e5c261e61e9a
iuventaas <55257470+iuventaas@users.noreply.github.com>
2020-07-07 23:23:48 +0300

    Update views.py

--- webform/mysite/web/views.py
+++ webform/mysite/web/views.py
@@ -38,7 +38,7 @@ def show(request):
     elements = r.hgetall(user)
     positions = []
     for key in elements:
-        positions.append(Position(key, elements[key]))
+        positions.append(Position(key[2:-1], elements[key][2:-1]))
 
     return render(request, 'web/show.html', {'positions': positions})
 
